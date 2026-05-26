879dd7094eda3cbf55e1e6d0e47c2e28cc80d62d
Victor Getty <gettyvictor@gmail.com>
2026-05-26 04:17:51 -0400

    Add: buy-and-sell-crypto - submission-15

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-15.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        l = 0
+        r = 1
+
+        maxP = 0
+
+        while (r < len(prices)):
+            profit = prices[r] - prices[l]
+            if profit > 0:
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+
+        return maxP
