c8c5154c42344aee660e80c3913a6f5e1de45445
Victor Getty <gettyvictor@gmail.com>
2026-06-08 00:42:39 -0400

    Add: same-binary-tree - submission-23

---
+++ Data Structures & Algorithms/same-binary-tree/submission-23.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
+       
+        stack =[[p, q]]
+
+        while stack:
+
+            nodep, nodeq  = stack.pop()
+            #print(nodep.val, nodeq.val)
+
+            if not nodep and not nodeq:
+                continue
+
+            if (not nodeq) or (not nodep) or (nodep.val != nodeq.val):
+                return False
+
+            # if (nodep.left.val != nodeq.left.val) or (nodep.right.val != nodep.right.val):
+            #     return False
+
+            #if nodep.left and nodeq.left:
+            stack.append([nodep.left, nodeq.left])
+
+            #if nodep.right and nodeq.right:
+            stack.append([nodep.right, nodeq.right])
+
+
+
+        return True
