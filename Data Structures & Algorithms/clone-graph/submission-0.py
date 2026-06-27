443fec0e0cc5830f5d5429766b3d7f48e01b697f
Victor Getty <gettyvictor@gmail.com>
2026-06-27 17:51:22 -0400

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.py
@@ -0,0 +1,38 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        res = {}
+
+        def dfs(node):
+            nonlocal res
+
+            if node in res:
+                return res[node]
+
+            if node is None:
+                return None
+
+            # if node == target:
+            #     return 1
+            copyNode = Node(val = node.val)
+            res[node] = copyNode
+
+            #visit.add(node)
+            for neighbor in node.neighbors:
+                copyNode.neighbors.append(dfs(neighbor))
+            #visit.remove(node)
+
+            return copyNode
+        
+        return dfs(node)
