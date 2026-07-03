bc3842f8d1e5d39372672fa2fe0fcfe742385ca0
Victor Getty <gettyvictor@gmail.com>
2026-07-03 19:10:50 -0400

    Add: valid-tree - submission-16

---
+++ Data Structures & Algorithms/valid-tree/submission-16.py
@@ -0,0 +1,51 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        
+        adjList = {key: [] for key in range(n)}
+
+        for src, dst in edges:
+            adjList[src].append(dst)
+            adjList[dst].append(src)
+            # if src < dst:
+            #     adjList[src].append(dst)
+            # else:
+            #     adjList[dst].append(src)
+        print(adjList)
+        def dfs(node, parent):
+
+            if node in path:
+                return False
+            #print(node, adjList[node])
+            path.add(node)
+
+            for neighbor in adjList[node]:
+                if neighbor == parent:
+                    continue
+                    
+                if not dfs(neighbor, node):
+                    return False
+
+            #adjList[node] = []
+            #path.remove(node)
+
+            return True
+
+        path = set()
+
+        
+        #for node in range(n):
+        # try:
+        #     node = edges[0][0]
+        # except:
+        #     return True
+
+        for node in range(n):
+            if node not in path:
+                if not dfs(node, -1):
+                    return False
+                
+        #print(len(path), path)
+        if len(edges) != n - 1:
+            return False
+
+        return True
